488187019f034b08099f4e874152a9bd02a92655
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-10-28 18:32:25 -0300

    aula 1

---
+++ JavaScript/main.js
@@ -0,0 +1,19 @@
+var x = 10
+var y = 15
+var z = x + y
+
+console.log(z); // -- permite outra atribuicao a variavel
+// ---
+let a = 5
+console.log(a);
+// ---
+const r = 45;
+console.log(r);
+const r = 54 // --- nao permite outra atribuicao, assim como o let
+
+f = 47;
+console.log(f);
+// ---- declaracao igual a var
+
+// ----------- operadores
+
