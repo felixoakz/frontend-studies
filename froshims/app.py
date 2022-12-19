9ef7a7ed32e4ed986d31a1548c84079976ba66d1
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-12-19 08:23:10 -0300

    update

--- froshims/app.py
+++ froshims/app.py
@@ -7,12 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route("/register", methods=["POST"])
 def register():
     
-    #validate submission
+    # validate submission
     if not request.form.get("name") or request.form.get("sport") not in ["Basketball", "Soccer", "Ultimate Frisbee"]:
         return render_template("failure.html")
 
     # confirm registration
     return render_template("success.html")
+
