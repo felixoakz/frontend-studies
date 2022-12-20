d5d12e97a440dd941555b33d90d7f339b8f8fe16
felixoakz <101996834+FelixOakz@users.noreply.github.com>
2022-12-20 20:50:13 -0300

    app bday

--- birthdays/app.py
+++ birthdays/app.py
@@ -26,13 +26,14 @@ def after_request(response):
 def index():
     if request.method == "POST":
 
-        # TODO: Add the user's entry into the database
+        name = request.form.get("name")
+        day = request.form.get("day")
+        month = request.form.get("month")
+
+        db.execute("INSERT INTO birthdays (name, day, month) VALUES (?, ?, ?)", name, day, month)
 
         return redirect("/")
 
     else:
-
-        # TODO: Display the entries in the database on index.html
-
-        return render_template("index.html")
-    
+        birthdays = db.execute("SELECT * FROM birthdays")
+        return render_template("index.html", birthdays=birthdays)
