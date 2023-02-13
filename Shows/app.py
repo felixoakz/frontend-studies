a148cc2739a6fdee4324a8dcf7e82084e4c4fbf8
felixoakz <felix@oakz.org>
2023-02-13 16:38:07 -0300

    shows app

--- Shows/app.py
+++ Shows/app.py
@@ -4,7 +4,31 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-db = SQL("sqlite:///shows.db")
+
+def execute_query(query, params=None, fetch=True):
+    """
+    -> a function to execute sqlite3 queries
+    :param query: sql querie with unnamed parameters (?) in place of variables
+    :param params: parameters in order and inside a TUPLE
+    :param fetch: set to True if querie awaits some return value. False otherwise.
+    :return: list of dictionary with items.
+    """
+    with sqlite3.connect("shows.db") as conn:
+        cursor = conn.cursor()
+
+        if params is None:
+            cursor.execute(query)
+
+        else:
+            cursor.execute(query, params)
+
+        if fetch:
+            columns = [col[0] for col in cursor.description]
+            result = [dict(zip(columns, row)) for row in cursor.fetchall()]
+            return result
+
+        else:
+            conn.commit()
 
 
 @app.route("/")
@@ -14,8 +38,7 @@ def index():
 
 @app.route("/search")
 def search():
-    shows = db.execute("SELECT * FROM shows WHERE title LIKE ?",
-                       "%" + request.args.get("q") + "%")
+    q = "%" + request.args.get("q") + "%"
+    shows = execute_query(
+        "SELECT * FROM shows WHERE title LIKE ?", (q,), fetch=True)
     return render_template("search.html", shows=shows)
-
-# finish the app
