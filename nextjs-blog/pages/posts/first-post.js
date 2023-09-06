ce6fa9cf64df5d57c80b1a98c6ffa1333fa04a18
felixoakz <felix@oakz.org>
2023-09-06 20:11:58 -0300

    next

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,13 +1,18 @@
+import Head from "next/head";
 import Link from "next/link";
+import Layout from "../../components/layout";
 
 const FirstPost = () => {
     return (
-        <>
+        <Layout>
+            <Head>
+                <title>First post</title>
+            </Head>
             <h1>First Post</h1>
             <h2>
                 <Link href="/">Back Home</Link>
             </h2>
-        </>
+        </Layout>
     )
 }
 
